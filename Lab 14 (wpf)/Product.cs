25cffa5dae8183d50968eb2fc4b8e35455ae901f
Андрей Мисюркеев <misyurckeev.andrey@yandex.ru>
2021-11-24 22:28:10 +0300

    Добавьте файлы проекта.

---
+++ Lab 14 (wpf)/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_14__wpf_
+{
+    public enum Category
+    {
+        Food,
+        Appliances
+    }
+    public class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Picture { get; set; }
+        public Category category { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
